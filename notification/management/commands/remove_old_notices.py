5258583f2e80afa1e77aa6ca47c8be4ef058a59c
Nathan K <me@madteckhead.dreamhosters.com>
2012-08-01 12:15:37 -0400

    Django1.4 with USE_TZ=True needs timezone aware datetimes
    Conflicts:
    
    	notification/management/commands/remove_old_notices.py

---
+++ notification/management/commands/remove_old_notices.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+from time import time
+from datetime import datetime
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.utils.timezone import make_aware, utc
+
+from notification.models import Notice
+
+
+class Command(BaseCommand):
+    """Remove old notices"""
+    help = __doc__
+
+    option_list = BaseCommand.option_list + (
+        make_option('--dry-run',
+            action='store_true',
+            dest='dryrun',
+            default=False,
+            help='Count the number of elements that would be deleted, without actually doing it'),
+        )
+
+    def handle(self, *args, **options):
+        dryrun = options['dryrun']
+        limit = time() - settings.NOTICES_MAX_AGE
+        limit = make_aware(datetime.fromtimestamp(limit), utc)
+        notices = Notice.objects.filter(added__lte=limit)
+        if dryrun:
+            count = notices.count()
+            print 'Total count of notices to be deleted: %d' % count
+        else:
+            notices.delete()
+
+
